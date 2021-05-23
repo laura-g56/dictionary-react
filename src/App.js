7eb8163a7652813416b3f1b995dc22c4888d861c
Laura G <laura_g56@hotmail.com>
2021-05-23 19:46:46 +1000

    more styling amendments

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo img-fluid" alt="logo" /> */}
-        <h1> Online Dictionary</h1>
-      </header>
-      <main>
-        <Dictionary defaultKeyword="dictionary"/>
-      </main>
-        <Footer />
+        <div className="card">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo img-fluid" alt="logo" /> */}
+          <h1> Online Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary defaultKeyword="dictionary"/>
+        </main>
+          <Footer />
+        </div>
       </div>
     </div>
   );
