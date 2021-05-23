2006018621d3fa577614490d71cdb732904fcc82
Laura G <laura_g56@hotmail.com>
2021-05-23 19:35:35 +1000

    Styling + default word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,10 @@ import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
 
-export default function Dictionary() {
-let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+let [keyword, setKeyword] = useState(props.defaultKeyword);
 let [results, setResults] = useState(null);
+let [loaded, setLoaded] = useState(false);
 
 function handleResponse(response) {
   setResults(response.data[0]) ;
@@ -15,6 +16,7 @@ function handleResponse(response) {
     event.preventDefault();
     search();
   }
+
 function search() {
   // documentation: https://dictionaryapi.dev/e
   let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
@@ -25,12 +27,29 @@ function search() {
   setKeyword(event.target.value);
   }
 
+  function load() {
+    setLoaded(true);
+    search()
+  }
+
+  if(loaded) {
   return (
-  <div className="dictionary">
-    <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a word..." onChange={handleKeywordChange}/>
-    </form>
+  <div className="dictionary search-form">
+    <form onSubmit={handleSubmit} className="input-group mb-4 col-12">
+      <input type="search" className="form-control" placeholder="Enter a word..." onChange={handleKeywordChange}/>
+        <button
+          className="btn btn-outline-secondary"
+          type="submit"
+          id="button-addon2"
+        >
+          <i className="fas fa-search"></i> Search
+        </button>
+      </form>
     <Results results={results} />
   </div>
   )
+  } else {
+      load();
+      return null
+  }
 }
