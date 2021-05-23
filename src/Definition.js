d5125d5ada34c14315f23e3928b9417656469a5e
Laura G <laura_g56@hotmail.com>
2021-05-23 19:35:17 +1000

    Definition styling

--- src/Definition.js
+++ src/Definition.js
@@ -9,7 +9,7 @@ export default function Definition(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
-          <div key={index}>
+          <div key={index} className="defined">
             <p>
               <strong> Definition:</strong>
               <br />
