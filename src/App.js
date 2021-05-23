d8e80e7225b34f90084c98aa5413af148142a473
Laura G <laura_g56@hotmail.com>
2021-05-23 19:34:55 +1000

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import logo from './logo.png';
 import './App.css';
 import Dictionary from "./Dictionary";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        {/* <img src={logo} className="App-logo img-fluid" alt="logo" /> */}
+        <h1> Online Dictionary</h1>
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="dictionary"/>
       </main>
-      <footer className="App-footer">
-        <small>
-          Coded by Laura Gumienik |{" "}
-          <a
-            className="github-ref"
-            href="https://github.com/laura-g56/dictionary-react"
-            target="_blank norefferer"
-          >
-            GitHub
-          </a>
-          {/* |{" "} */}
-          {/* <a
-            className="github-ref"
-            href="https://suspicious-meninsky-847725.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify
-          </a> */}
-        </small>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
